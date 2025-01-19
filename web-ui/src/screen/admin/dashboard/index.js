9d8e9396ea2db549e2e9c397b6dc3a969aeea0b8
Sovatey Hen <sovatey.hen@vital.com.kh>
2025-01-19 14:32:11 +0700

    .

---
+++ web-ui/src/screen/admin/dashboard/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Layout, Divider } from 'antd';
+import Sidebar from '../../sidebar';
+import { useLocation } from 'react-router-dom';
+
+const { Content } = Layout;
+
+const DashboardScreen = () => {
+    const location = useLocation();
+    const username = location.state;
+
+    return (
+        <Layout style={{ minHeight: '100vh' }}>
+            {<Sidebar username={username} />}
+            <Layout >
+                <Content
+                    style={{
+                        padding: 24,
+                        margin: 0,
+                        minHeight: 280,
+                        background: '#fff',
+                    }}
+                >
+                    <h2>Dashboard</h2>
+                    <Divider />
+                    <p>This is the Dashboard Screen</p>
+                </Content>
+            </Layout>
+        </Layout>
+    );
+};
+
+export default DashboardScreen;
---
+++ web-ui/src/screen/login.js
@@ -0,0 +1,109 @@
+import React from "react";
+import { useNavigate } from 'react-router-dom';
+import { Button, Form, Input, Typography, Card, message } from 'antd';
+import Snowfall from 'react-snowfall';
+import logo from '../assets/images/logo.jpg';
+
+
+const LoginScreen = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = (values) => {
+    const { username, password } = values;
+
+    if (username === 'admin' && password === '123456') {
+      message.success('Login successful!');
+      navigate('/dashboard');  // Navigate to dashboard after login
+    } else {
+      message.error('Invalid username or password!');
+    }
+  };
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        background: 'linear-gradient(to bottom, #e0f7fa, #ffffff)',
+        position: 'relative',
+      }}
+    >
+      <Snowfall snowflakeCount={200} />
+
+      <div
+        style={{
+          display: 'flex',
+          boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+          borderRadius: '10px',
+          overflow: 'hidden',
+        }}
+      >
+        <div
+          style={{
+            width: 300,
+            backgroundColor: '#f0f8ff',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <img
+            src={logo}
+            alt="Login Illustration"
+            style={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+            }}
+          />
+        </div>
+        <Card
+          style={{
+            width: 400,
+            padding: '24px',
+          }}
+          bordered={false}
+        >
+          <Typography.Title level={3} style={{ textAlign: 'center' }}>
+            Login
+          </Typography.Title>
+          <Form
+            layout="vertical"
+            onFinish={handleLogin}
+            initialValues={{ username: '', password: '' }}
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[{ required: true, message: 'Please enter your username!' }]}
+            >
+              <Input placeholder="Enter your username" />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{ required: true, message: 'Please enter your password!' }]}
+            >
+              <Input.Password placeholder="Enter your password" />
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit" style={{ width: '100%', backgroundColor: '#ffaead' }}>
+                Log In
+              </Button>
+            </Form.Item>
+          </Form>
+          <div style={{ textAlign: 'center', marginTop: 16 }}>
+            <Typography.Text type="secondary">
+              Don't have an account? <a href="/register" style={{ color: '#ffaead' }}>Sign Up</a>
+            </Typography.Text>
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default LoginScreen;
